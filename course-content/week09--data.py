34c4f0062fff21024bda0016079af5de8627748a
George McNinch <gmcninch@gmail.com>
2025-03-15 17:02:23 -0400

    update on 2025-03-15 17:02:22 by george@calliope

--- course-content/week09--data.py
+++ course-content/week09--data.py
@@ -25,13 +25,34 @@ states = [ 'bull', 'bear', 'recess' ]
 
 def get_next_week_probs(a,state):
     inds = get_indices(a,state)
-    n = if (len(a)-1) in inds:
-            len(inds)-1
-        else:
-            len(inds)
+    if (len(a)-1) in inds:
+        n=len(inds)-1
+    else:
+        n=len(inds)
     N = len(a)
 
     # get the list of states for weeks following weeks of the input state
     nxt = { ns: [ i for i in inds if i+1<N and a[i+1] == st] for ns in states }
     
     return { nxt[ns]/n for ns in states }
+
+
+pp = np.array([[0.80 , 0.15 , 0.25],
+               [0.175, 0.80 , 0.25],
+               [0.025, 0.05 , 0.50]])
+
+def gen(pp,c):
+    i = states.index(c)
+    ps=pp[:,i]
+    print(ps)
+    return str(rng.choice(states,p=ps))
+
+res = []
+cur = str(rng.choice(states))
+for _ in range(52*20):
+    cur = gen(pp,cur)
+    res.append(cur)
+
+
+write_json('week09--data-market-new.json',list(map(lambda x: str(x),res)))
+    
